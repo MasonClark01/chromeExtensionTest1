7f9eeeb35d49dd5cf85f731c0f6f550d4c6dca49
Mason Clark <kryptonnova1@gmail.com>
2024-07-10 13:18:48 -0400

    AddedCancelNoteFunc&TidiedUp

--- testDropdown/main.js
+++ testDropdown/main.js
@@ -6,8 +6,8 @@ let notes = {
 
 }
 
-//looks for the dom element that stores the store name on manager dashboard.
-const fetchData = () =>{
+//looks for the dom elements that store the location name & user name on manager dashboard.
+const fetchData = () => {
     const storeName = document.querySelector(".text-wrap")
     const operatorName = document.querySelector(".text-muted")
     return [storeName.innerText, operatorName.innerText]
@@ -15,12 +15,10 @@ const fetchData = () =>{
 
 //Queries tabs on opened chrome page looking for the active tab and running fetchStoreId on that tab.
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    console.log("Execute Script");
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
       func: fetchData
     }, (result) => {
-      console.log(result[0].result[0]);
       storeIdentifier.innerText = `Notes for: ${result[0].result[0]}`
       nameIdentifier = `${result[0].result[1]}`
     });
@@ -48,7 +46,17 @@ const postNote = (title, body) => {
 }
 
 //New Feature: Delete Note and Cancel Note (have to change notescontainer class "adding" and addbutton classes "inactive" & "active")
+const deleteNote = (arg) => {
+    
+}
 
+const cancelNote = (arg) => {
+    notesContainer.classList.remove("adding")
+    addButton.classList.remove("inactive")
+    addButton.classList.add("active")
+    arg.remove()
+    renderNotes()
+}
 //New Feature: If the user is a deployment engineer allow posting to "global notes", otherwise post based on location.
 
 //Creates form for creating a new note and removes add new note button
@@ -65,11 +73,16 @@ const createNote = () => {
         const body = document.createElement("input")
         const post = document.createElement("button")
         const cancel = document.createElement("button")
+        post.classList.add("buttonClass")
+        cancel.classList.add("buttonClass")
 
-        head.placeholder = "Title"
-        body.placeholder = "Body"
+        head.placeholder = "Title (75 Character Limit)"
+        head.maxLength = 75
+        body.placeholder = "Body (300 Character Limit)"
+        body.maxLength = 300
         post.innerText = "Post"
         cancel.innerText = "Cancel"
+        cancel.addEventListener("click", (e)=>cancelNote(newNoteDiv))
         post.addEventListener("click", (e)=>postNote(head, body))
 
         newNoteDiv.appendChild(cancel)
@@ -81,15 +94,14 @@ const createNote = () => {
 }
 addButton.addEventListener("click", createNote)
 
-const deleteNote = (arg) =>{
-
-}
 //handles creating/refreshing the list of notes populated from the received object
-const renderNotes = () =>{
+const renderNotes = () => {
     notesContainer.innerHTML = ""
     for(let note in notes){
         const noteDiv = document.createElement("div")
+        const topDiv = document.createElement("div")
         noteDiv.classList.add("noteClass")
+        topDiv.classList.add("noteTopClass")
 
         const noteTitle = document.createElement("h4")
         const noteBody = document.createElement("p")
@@ -107,10 +119,11 @@ const renderNotes = () =>{
             author.innerText = "Placeholder Name"
         }
 
+        topDiv.appendChild(author)
+        topDiv.appendChild(deleteButton)
+        noteDiv.appendChild(topDiv)
         noteDiv.appendChild(noteTitle)
         noteDiv.appendChild(noteBody)
-        noteDiv.appendChild(author)
-        noteDiv.appendChild(deleteButton)
 
         notesContainer.appendChild(noteDiv)
     }
